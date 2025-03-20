0f993022154cd35abc2e037b2023f6b64b2e5de5
TerkumaAmos <agberterkuma2020@gmail.com>
2025-03-20 10:54:48 +0100

    new

--- lib/medidate.dart
+++ lib/medidate.dart
@@ -6,20 +6,23 @@ class medidate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFECF4E2),
+      backgroundColor: Color(0xFFECF4E2),
       appBar: AppBar(
-        backgroundColor: const Color(0xFFECF4E2),
-        title: const Text(""), // Optional: Add a title if needed, or leave empty
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0), // Optional: Add some padding from the edge
-            child: Image(
-              image: const AssetImage("assets/amos12.png"),
-              width: 40, // Optional: Adjust size as needed
-              height: 40,
+        backgroundColor: Color(0xFFECF4E2),
+        title:  Stack(
+
+          children: [
+            Positioned(
+
+              child: Image(
+                image: AssetImage(
+                    "assets/amos12.png",
+                ),
+
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
