b6e6c63d1b32a585dd8778192be7438294869444
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-29 10:50:33 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:harmony_hush/onboard_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
