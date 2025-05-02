8fdde13fb0f9222189d5bf8987a5e8f6b1afb88a
TerkumaAmos <agberterkuma2020@gmail.com>
2025-05-02 13:14:21 +0100

    firebase

---
+++ lib/services/auth.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in anonmousyly
+
+  Future signInAnon() async {}
+
+  // sign in with
+}
