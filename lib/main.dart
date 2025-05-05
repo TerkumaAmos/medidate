bd6e3b003065e1eca978fea0f0469bab712bc5ea
TerkumaAmos <agberterkuma2020@gmail.com>
2025-05-05 14:18:24 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:harmony_hush/onboard_screen.dart';
 import 'package:harmony_hush/practice.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final options = FirebaseOptions(
+    apiKey: "AIzaSyBnfLWPL1KBOfBV5BpVFi31qWYYnxh504k",
+    appId: "com.example.harmony_hush",
+    messagingSenderId: "your-messaging-sender-id",
+    projectId: "harmonyhush-3f558",
+  );
+  await Firebase.initializeApp(options: options);
 
   runApp(const MyApp());
 }
@@ -15,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Practice(),
+      home: OnboardScreen(),
     );
   }
 }
